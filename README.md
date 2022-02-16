12aa46f729c4e2d0fc933ee5331466399cfd10d9
Daniel Manila <dmv@manilas.net>
2022-02-16 14:14:37 -0500

    Setup project to new base state for the workshop

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# Pure-Javascript-Todo-list
-Pure Javascript Todo list with browser localstorage functionality. Saves the todolist even if the page reloads.
+# Firebase-Todo-example
+Pure Javascript Todo list showing interaction with Firebase. Actual list functionality comes from https://github.com/devmayor/Pure-Javascript-Todo-list
 
-Check out the demo
-http://bit.ly/2uLq35x
+## Running the code
+
+First, be sure to set up a proper firestore setup config in `index.html` (starting on line 71). The one in this repository is on my personal account and I may delete it at any time. For learning purposes you can just set up your own [firebase project](https://firebase.google.com/docs/web/setup)
+
+Then simply start an HTTP server in this directory. I personally use python:
+`python -m http.server` (requires python3)
+
+## Stepping through the workshop
+
+You can step through different by checkout out the following git tags:
+
+`git checkout base`: starts the workshop with no firebase added, except the project config
+`git checkout auth`: jumps to the authentication commit
+`git checkout firestore`: jumps to the firestore commit
+
+If you want to try and code through on your own, you can start a new branch at any of the tag points with `git checkout tags/{tagname} -b {branchname}`. Replace `{tagname}` with the name of the tag you want to start from and `{branchname}` with the new name of your branch.
--- js/main.js
+++ js/main.js
@@ -11,41 +11,43 @@ var listArray = [];
 //declare addToList function
 
 function listItemObj(content, status) {
-    this.content = '';
-    this.status = 'incomplete';
+    return {
+        content: "",
+        status: "incomplete"
+    };
 }
-var changeToComp = function(){
+var changeToComp = function () {
     var parent = this.parentElement;
     console.log('Changed to complete');
     parent.className = 'uncompleted well';
     this.innerText = 'Incomplete';
-    this.removeEventListener('click',changeToComp);
-    this.addEventListener('click',changeToInComp);
-    changeListArray(parent.firstChild.innerText,'complete');
+    this.removeEventListener('click', changeToComp);
+    this.addEventListener('click', changeToInComp);
+    changeListArray(parent.firstChild.innerText, 'complete');
 
 }
 
-var changeToInComp = function(){
+var changeToInComp = function () {
     var parent = this.parentElement;
     console.log('Changed to incomplete');
     parent.className = 'completed well';
     this.innerText = 'Complete';
-    this.removeEventListener('click',changeToInComp);
-    this.addEventListener('click',changeToComp);
+    this.removeEventListener('click', changeToInComp);
+    this.addEventListener('click', changeToComp);
 
-    changeListArray(parent.firstChild.innerText,'incomplete');
+    changeListArray(parent.firstChild.innerText, 'incomplete');
 
 }
 
-var removeItem = function(){
+var removeItem = function () {
     var parent = this.parentElement.parentElement;
     parent.removeChild(this.parentElement);
 
     var data = this.parentElement.firstChild.innerText;
-    for(var i=0; i < listArray.length; i++){
+    for (var i = 0; i < listArray.length; i++) {
 
-        if(listArray[i].content == data){
-            listArray.splice(i,1);
+        if (listArray[i].content == data) {
+            listArray.splice(i, 1);
             refreshLocal();
             break;
         }
@@ -55,11 +57,11 @@ var removeItem = function(){
 }
 
 //function to change the todo list array
-var changeListArray = function(data,status){
+var changeListArray = function (data, status) {
 
-    for(var i=0; i < listArray.length; i++){
+    for (var i = 0; i < listArray.length; i++) {
 
-        if(listArray[i].content == data){
+        if (listArray[i].content == data) {
             listArray[i].status = status;
             refreshLocal();
             break;
@@ -68,7 +70,7 @@ var changeListArray = function(data,status){
 }
 
 //function to chage the dom of the list of todo list
-var createItemDom = function(text,status){
+var createItemDom = function (text, status) {
 
     var listItem = document.createElement('li');
 
@@ -78,21 +80,21 @@ var createItemDom = function(text,status){
 
     var itemIncompBtn = document.createElement('button');
 
-    listItem.className = (status == 'incomplete')?'completed well':'uncompleted well';
+    listItem.className = (status == 'incomplete') ? 'completed well' : 'uncompleted well';
 
     itemLabel.innerText = text;
     itemCompBtn.className = 'btn btn-success';
-    itemCompBtn.innerText = (status == 'incomplete')?'Complete':'Incomplete';
-    if(status == 'incomplete'){
-        itemCompBtn.addEventListener('click',changeToComp);
-    }else{
-        itemCompBtn.addEventListener('click',changeToInComp);
+    itemCompBtn.innerText = (status == 'incomplete') ? 'Complete' : 'Incomplete';
+    if (status == 'incomplete') {
+        itemCompBtn.addEventListener('click', changeToComp);
+    } else {
+        itemCompBtn.addEventListener('click', changeToInComp);
     }
 
 
     itemIncompBtn.className = 'btn btn-danger';
     itemIncompBtn.innerText = 'Delete';
-    itemIncompBtn.addEventListener('click',removeItem);
+    itemIncompBtn.addEventListener('click', removeItem);
 
     listItem.appendChild(itemLabel);
     listItem.appendChild(itemCompBtn);
@@ -101,43 +103,43 @@ var createItemDom = function(text,status){
     return listItem;
 }
 
-var refreshLocal = function(){
+var refreshLocal = function () {
     var todos = listArray;
     localStorage.removeItem('todoList');
     localStorage.setItem('todoList', JSON.stringify(todos));
 }
 
-var addToList = function(){
+var addToList = function () {
     var newItem = new listItemObj();
     newItem.content = addInput.value;
     listArray.push(newItem);
     //add to the local storage
     refreshLocal();
     //change the dom
-    var item = createItemDom(addInput.value,'incomplete');
+    var item = createItemDom(addInput.value, 'incomplete');
     todoList.appendChild(item);
     addInput.value = '';
 }
 
 //function to clear todo list array
-var clearList = function(){
+var clearList = function () {
     listArray = [];
     localStorage.removeItem('todoList');
     todoList.innerHTML = '';
 
 }
 
-window.onload = function(){
+window.onload = function () {
     var list = localStorage.getItem('todoList');
 
     if (list != null) {
-        todos = JSON.parse(list);
+        var todos = JSON.parse(list);
         listArray = todos;
 
-        for(var i=0; i<listArray.length;i++){
+        for (var i = 0; i < listArray.length; i++) {
             var data = listArray[i].content;
 
-            var item = createItemDom(data,listArray[i].status);
+            var item = createItemDom(data, listArray[i].status);
             todoList.appendChild(item);
         }
 
@@ -145,5 +147,5 @@ window.onload = function(){
 
 };
 //add an event binder to the button
-addButton.addEventListener('click',addToList);
-clearButton.addEventListener('click',clearList);
+addButton.addEventListener('click', addToList);
+clearButton.addEventListener('click', clearList);
